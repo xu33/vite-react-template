f9c0736a336a8b10d51446342635b2c4bc163305
nanshi <nan.shi@upchina.com>
2025-09-26 13:38:51 +0800

    feat:test

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <>
       <div>
+        <a>Hello workers</a>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
